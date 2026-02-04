d94e3fe9d758c7680b232616d47635a91a7d2a41
rehan <rehanahmad3775@gmail.com>
2026-02-04 15:09:33 +0500

    added javascript functionality

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ icon.addEventListener("click", ()=>{
         document.getElementById("bar").className= "fa-solid fa-bars";
     }
 })
+
+let shops = document.getElementById("shops");
+let reviews = document.getElementById("reviews");
+let blogs = document.getElementById("blogs");
+let contacts = document.getElementById("contacts");
+
+shops.addEventListener("click", ()=>{
+    shops.style.color="#00ffff";
+    reviews.style.color="white"; 
+    blogs.style.color="white";
+    contacts.style.color="white"; 
+});
+
+reviews.addEventListener("click", ()=>{
+    reviews.style.color="#00ffff";
+    shops.style.color="white"; 
+    blogs.style.color="white";
+    contacts.style.color="white";
+});
+
+blogs.addEventListener("click", ()=>{
+    blogs.style.color="#00ffff";
+    reviews.style.color="white"; 
+    shops.style.color="white";
+    contacts.style.color="white"; 
+});
+
+contacts.addEventListener("click", ()=>{
+    contacts.style.color="#00ffff";
+    shops.style.color="white"; 
+    blogs.style.color="white";
+    reviews.style.color="white"; 
+});

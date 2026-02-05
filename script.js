2d7285e971cd4a5e3c15bc8b380e3a223f80161d
rehan <rehanahmad3775@gmail.com>
2026-02-05 13:04:32 +0500

    solve mistak in ankr tag

--- script.js
+++ script.js
@@ -15,32 +15,34 @@ icon.addEventListener("click", ()=>{
 let shops = document.getElementById("shops");
 let reviews = document.getElementById("reviews");
 let blogs = document.getElementById("blogs");
-let contacts = document.getElementById("contacts");
+let contact = document.getElementById("contact");
 
 shops.addEventListener("click", ()=>{
     shops.style.color="#00ffff";
     reviews.style.color="white"; 
     blogs.style.color="white";
-    contacts.style.color="white"; 
+    contact.style.color="white"; 
 });
 
 reviews.addEventListener("click", ()=>{
     reviews.style.color="#00ffff";
     shops.style.color="white"; 
     blogs.style.color="white";
-    contacts.style.color="white";
+    contact.style.color="white";
 });
 
 blogs.addEventListener("click", ()=>{
     blogs.style.color="#00ffff";
     reviews.style.color="white"; 
     shops.style.color="white";
-    contacts.style.color="white"; 
+    contact.style.color="white"; 
 });
 
-contacts.addEventListener("click", ()=>{
-    contacts.style.color="#00ffff";
-    shops.style.color="white"; 
+contact.addEventListener("click", ()=>{
+    contact.style.color="#00ffff"; 
     blogs.style.color="white";
     reviews.style.color="white"; 
+    shops.style.color="white";
 });
+
+
